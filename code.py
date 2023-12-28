1c11ee015e3768aa91777b6ea7014b63c2e16428
m-brahim <155063113+m-brahim@users.noreply.github.com>
2023-12-28 11:33:02 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,18 @@
 import streamlit as st
-import pandas
+import folium
 
-st.title("This is my streamlit app")
+# Titre de l'application
+st.title("Visualisation de point avec Folium dans Streamlit")
+
+# Coordonnées du point à afficher
+latitude = 39.949610
+longitude = -75.150282
+
+# Création de la carte Folium
+m = folium.Map(location=[latitude, longitude], zoom_start=16)
+
+# Ajout d'un marqueur sur la carte
+folium.Marker([latitude, longitude], popup="Votre point").add_to(m)
+
+# Affichage de la carte dans Streamlit
+st.write(m)
