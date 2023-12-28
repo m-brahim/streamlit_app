bf9e75976f53faf1144446da2972b8e38a6eb91b
m-brahim <155063113+m-brahim@users.noreply.github.com>
2023-12-28 11:14:24 +0100

    Update code.py

--- code.py
+++ code.py
@@ -4,5 +4,5 @@ from streamlit_folium import st_folium
 
 dot = (39.949610, -75.150282)
 m = folium.Map(location=dot, zoom_start=16)
-folium.Marker(location, popup="Liberty Bell", tooltip="Liberty Bell").add_to(m)
+folium.Marker(dot, popup="Liberty Bell", tooltip="Liberty Bell").add_to(m)
 st_folium(m, width=725)
