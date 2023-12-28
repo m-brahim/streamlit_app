8a52f0d55ad6e3ff20fdfbf716d79fc35f876134
m-brahim <155063113+m-brahim@users.noreply.github.com>
2023-12-28 17:54:51 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,19 +1,3 @@
 import streamlit as st
-import snowflake.connector
-import pandas as pd
 
-# Remplacez ces valeurs par vos propres informations de connexion
-username = "BMEGDOUD@SYNERGY.FR"
-password = "Azert2609*"
-account = "synergy.eu-west-1.snowflakecomputing.com"
-database = "DEMODB"
-warehouse = "SYNERGY"
-
-# Établir la connexion à Snowflake
-conn = snowflake.connector.connect(
-    user=username,
-    password=password,
-    account=account,
-    warehouse=warehouse,
-    database=database
-)
+st.title('This is my streamlit app')
