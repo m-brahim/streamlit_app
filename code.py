52573959745ce9097b65dd0fd1c57b8c98c5795a
m-brahim <155063113+m-brahim@users.noreply.github.com>
2023-12-29 12:52:53 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,8 @@
+import folium
+import streamlit as st
+
+from streamlit_folium import st_folium
+
 # center on Liberty Bell, add marker
 m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
 folium.Marker(
