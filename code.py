6b80d5f863e2a1b83443ee6c39c56ca583c11dda
m-brahim <155063113+m-brahim@users.noreply.github.com>
2023-12-29 11:33:19 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,16 +1,5 @@
-# Import python packages
-import branca
 import folium
-import folium.plugins
-import streamlit as st
-import streamlit.components.v1 as components
-from jinja2 import UndefinedError
-from streamlit_folium import st_folium
 
-m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-folium.Marker(
-    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
-).add_to(m)
+m = folium.Map(location=(45.5236, -122.6750))
 
-# call to render Folium map in Streamlit
-st_data = st_folium(m, width=725)
+m
