bd4f6d33aea82c7db61379dda83ded6339f94b0f
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-02 10:35:13 +0100

    Update code.py

--- code.py
+++ code.py
@@ -9,4 +9,7 @@ col1, col2, col3, col4 = st.columns(4)
 # Contenu de chaque colonne
 with col1:
     st.header("Liste des commerces")
-    st.write(commerces)
+    
+    # Utilisez st.markdown pour afficher la liste en Markdown
+    markdown_liste_commerces = "\n".join(f"- {commerce}" for commerce in commerces)
+    st.markdown(markdown_liste_commerces)
