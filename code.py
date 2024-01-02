00abe21409f8f4e76f3b6f79ee83474f0893dffb
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-02 10:28:27 +0100

    Update code.py

--- code.py
+++ code.py
@@ -5,61 +5,41 @@ import pandas as pd
 
 from streamlit_elements import elements, mui, html
 
-# Create a frame where Elements widgets will be displayed.
-#
-# Elements widgets will not render outside of this frame.
-# Native Streamlit widgets will not render inside this frame.
-#
-# elements() takes a key as parameter.
-# This key can't be reused by another frame or Streamlit widget.
+with elements("dashboard"):
 
-with elements("new_element"):
+    # You can create a draggable and resizable dashboard using
+    # any element available in Streamlit Elements.
 
-    # Let's create a Typography element with "Hello world" as children.
-    # The first step is to check Typography's documentation on MUI:
-    # https://mui.com/components/typography/
-    #
-    # Here is how you would write it in React JSX:
-    #
-    # <Typography>
-    #   Hello world
-    # </Typography>
+    from streamlit_elements import dashboard
 
-    mui.Typography("Hello world")
+    # First, build a default layout for every element you want to include in your dashboard
 
+    layout = [
+        # Parameters: element_identifier, x_pos, y_pos, width, height, [item properties...]
+        dashboard.Item("first_item", 0, 0, 2, 2),
+        dashboard.Item("second_item", 2, 0, 2, 2, isDraggable=False, moved=False),
+        dashboard.Item("third_item", 0, 2, 1, 1, isResizable=False),
+    ]
 
-with elements("multiple_children"):
+    # Next, create a dashboard layout using the 'with' syntax. It takes the layout
+    # as first parameter, plus additional properties you can find in the GitHub links below.
 
-    # You have access to Material UI icons using: mui.icon.IconNameHere
-    #
-    # Multiple children can be added in a single element.
-    #
-    # <Button>
-    #   <EmojiPeople />
-    #   <DoubleArrow />
-    #   Hello world
-    # </Button>
+    with dashboard.Grid(layout):
+        mui.Paper("First item", key="first_item")
+        mui.Paper("Second item (cannot drag)", key="second_item")
+        mui.Paper("Third item (cannot resize)", key="third_item")
 
-    mui.Button(
-        mui.icon.EmojiPeople,
-        mui.icon.DoubleArrow,
-        "Button with multiple children"
-    )
+    # If you want to retrieve updated layout values as the user move or resize dashboard items,
+    # you can pass a callback to the onLayoutChange event parameter.
 
-    # You can also add children to an element using a 'with' statement.
-    #
-    # <Button>
-    #   <EmojiPeople />
-    #   <DoubleArrow />
-    #   <Typography>
-    #     Hello world
-    #   </Typography>
-    # </Button>
+    def handle_layout_change(updated_layout):
+        # You can save the layout in a file, or do anything you want with it.
+        # You can pass it back to dashboard.Grid() if you want to restore a saved layout.
+        print(updated_layout)
 
-    with mui.Button:
-        mui.icon.EmojiPeople()
-        mui.icon.DoubleArrow()
-        mui.Typography("Button with multiple children")
-
+    with dashboard.Grid(layout, onLayoutChange=handle_layout_change):
+        mui.Paper("First item", key="first_item")
+        mui.Paper("Second item (cannot drag)", key="second_item")
+        mui.Paper("Third item (cannot resize)", key="third_item")
 
 
