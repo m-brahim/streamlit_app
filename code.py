775689e711802e895799d0b4ff4c638bcd8b2635
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-02 11:19:22 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,14 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-
-st.title("Tableau de bords Streamlit - Suivi des ventes 
+st.title("Tableau de bords Streamlit")
 
 # Données
 data = {
     "Nom": ["Carrefour", "Auchan", "Leclerc"],
     "Nombre de magasins en France": [253, 136, 585],
-    
 }
 
 df = pd.DataFrame(data)
@@ -18,13 +16,13 @@ col1, col2 = st.columns(2)
 
 # Contenu de chaque colonne
 with col1:
-    st.header("Données sur les magasins")
+    st.header("Données sur les commerces")
 
-    # Afficher le tableau de données
-    st.write(df)
+    # Afficher le tableau de données sans le nombre de magasins
+    st.write(df[["Nom"]])
 
 with col2:
     st.header("Graphique en barres")
 
-    # Afficher le graphique en barres
-    st.bar_chart(df.set_index("Enseigne"))
+    # Afficher le graphique en barres avec le "Nom" comme étiquettes d'axe et le nombre de magasins comme valeurs
+    st.bar_chart(df.set_index("Nom"), use_container_width=True)
