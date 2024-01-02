9737362357406f3b4eaa41e1de1782144015cfd3
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-02 17:06:22 +0100

    Update 1_pays.py

--- pages/1_pays.py
+++ pages/1_pays.py
@@ -2,18 +2,12 @@ import streamlit as st
 import pandas as pd
 
 # Titre de la page
+
 st.set_page_config("Suivi géographique des ventes", page_icon="", layout="wide")
-
-# Diviser la page en 10 colonnes
-col1, _, _, _, _, _, _, _, _, _ = st.columns(10)
-
-# Sous-titre dans la première colonne
-col1.subheader("Suivi géographique des ventes")
+st.subheader("Suivi géographique des ventes")
 
 url = "Exemple - Hypermarché_Achats.csv"
 
 df = pd.read_csv(url, delimiter=";")
 
-# Afficher le dataframe dans une des colonnes (ici, la deuxième colonne)
-col2.write(df)
-
+st.write(df)
