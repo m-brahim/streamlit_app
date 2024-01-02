3992f4a8eba8f01a4b22be41dd4780ab78bbb587
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-02 10:32:43 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,45 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-# First, import the elements you need
+commerces = ["Auchan", "Carrefour", "Leclerc", "Lidl", "Aldi"]
 
-from streamlit_elements import elements, mui, html
+# Diviser la page en 4 colonnes
+col1, col2, col3, col4 = st.columns(4)
 
-with elements("dashboard"):
-
-    # You can create a draggable and resizable dashboard using
-    # any element available in Streamlit Elements.
-
-    from streamlit_elements import dashboard
-
-    # First, build a default layout for every element you want to include in your dashboard
-
-    layout = [
-        # Parameters: element_identifier, x_pos, y_pos, width, height, [item properties...]
-        dashboard.Item("first_item", 0, 0, 2, 2),
-        dashboard.Item("second_item", 2, 0, 2, 2, isDraggable=False, moved=False),
-        dashboard.Item("third_item", 0, 2, 1, 1, isResizable=False),
-    ]
-
-    # Next, create a dashboard layout using the 'with' syntax. It takes the layout
-    # as first parameter, plus additional properties you can find in the GitHub links below.
-
-    with dashboard.Grid(layout):
-        mui.Paper("First item", key="first_item")
-        mui.Paper("Second item (cannot drag)", key="second_item")
-        mui.Paper("Third item (cannot resize)", key="third_item")
-
-    # If you want to retrieve updated layout values as the user move or resize dashboard items,
-    # you can pass a callback to the onLayoutChange event parameter.
-
-    def handle_layout_change(updated_layout):
-        # You can save the layout in a file, or do anything you want with it.
-        # You can pass it back to dashboard.Grid() if you want to restore a saved layout.
-        print(updated_layout)
-
-    with dashboard.Grid(layout, onLayoutChange=handle_layout_change):
-        mui.Paper("First item", key="first_item")
-        mui.Paper("Second item (cannot drag)", key="second_item")
-        mui.Paper("Third item (cannot resize)", key="third_item")
-
-
+# Contenu de chaque colonne
+with col1:
+    st.header("Liste des commerces")
+    for i in commerces:
+        st.write(commerces)
