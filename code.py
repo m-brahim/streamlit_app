510f9f164584130fcf9129d479ee98f044a74619
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-02 13:38:54 +0100

    Update code.py

--- code.py
+++ code.py
@@ -33,7 +33,8 @@ logo_path = logo_paths.get(selected_commerce, "Chemin par défaut si le logo n'e
 col1, col2, col3 = st.columns(3)
 
 # Métrique 1 - Entreprise avec logo
-col1.image(logo_path, width=100, caption=selected_commerce)
+col1.metric("Enseigne")
+col1.image(logo_path, width=100)
 
 # Métrique 2 - Nombre de Magasins
 col2.metric("Nombre de Magasins", filtered_data["Nombre de magasins"].values[0])
