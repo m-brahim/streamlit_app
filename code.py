858c1375d60f7c2adc61abb07a8f8f68c887e1c7
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-02 09:58:52 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,16 @@
 import streamlit as st
-
+import pandas
 
 selection = st.sidebar.radio("Aller à :", ["Commerces", "Activité"])
+
+if selection == "Commerces":
+    # Titre de la page
+    st.title(":green[Liste des commerces]")
+    file_path = st.file_uploader("Sélectionnez un fichier CSV", type=["csv"])
+    if file_path is not None:
+    # Lire le fichier CSV
+    df = pd.read_csv(file_path)
+    # Afficher le DataFrame
+    st.write(df)
+
 
