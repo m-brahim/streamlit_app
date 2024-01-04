fa4fe245ddd0a35078567c6aae96e859318f12f4
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-04 17:01:25 +0100

    Update 1_pays.py

--- pages/1_pays.py
+++ pages/1_pays.py
@@ -59,10 +59,10 @@ st.header("")
 # Visualisation
 st.subheader("Visualisation")
 
-col_space, col_pie, col_space = st.columns([1, 3, 1])
+col_space, col_pie, col_space = st.columns([1, 2, 1])
 
-with col_pie:
-    st.markdown("<h3 style='text-align: center;'>Quantités vendues par catégorie</h3>", unsafe_allow_html=True)
+with col_pie :
+    st.subheader("Quantités vendues par catégorie")
     # Calculer les quantités vendues par catégorie pour le pays sélectionné
     quantity_by_category = filtered_data.groupby('Catégorie')['Quantité'].sum().reset_index()
     # Créer le graphique en secteur avec Plotly Express
@@ -70,9 +70,9 @@ with col_pie:
     st.plotly_chart(fig, use_container_width=True)
     
 
-col_space2, col_map, col_space2 = st.columns([1, 3, 1])
+col_space2, col_map, col_space2 = st.columns([1, 2, 1])
 
-with col_map:
-    st.markdown("<h3 style='text-align: center;'>Carte du monde</h3>", unsafe_allow_html=True)
+with col_map : 
+    st.subheader("Carte du monde")
     m = folium.Map(location=[0, 0], zoom_start=1.5)
-    st_folium(m, width=725, height=500)
+    st_folium(m, width=725, height = 500)
