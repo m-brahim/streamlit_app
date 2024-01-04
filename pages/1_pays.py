21996ad3aaf4a05db04c38141182b3322d3a2848
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-04 16:03:25 +0100

    Update 1_pays.py

--- pages/1_pays.py
+++ pages/1_pays.py
@@ -76,3 +76,4 @@ col_space2, col_map, col_space2 = st.columns([1, 3, 1])
 with col_map : 
     st.subheader("Carte du monde")
     m = folium.Map(location=[0, 0], zoom_start=0)
+    st_folium(m, width=725, height = 500)
