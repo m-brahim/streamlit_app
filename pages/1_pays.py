a258bfd076a549f25f6d3ba1aa468dbabf260e39
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-04 11:50:52 +0100

    Update 1_pays.py

--- pages/1_pays.py
+++ pages/1_pays.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 url = "Exemple - Hypermarché_Achats.csv"
 
@@ -57,3 +58,15 @@ st.header("")
 # Ajouter le 3ème titre 
 st.subheader("Visualisation")
 
+# Liste déroulante pour la sélection de catégorie
+selected_category = st.selectbox("Sélectionnez une catégorie", df['Catégorie'].unique())
+
+# Filtrer les données par catégorie
+filtered_data_category = df[df['Catégorie'] == selected_category]
+
+# Calculer les quantités vendues par pays pour la catégorie sélectionnée
+quantity_by_country = filtered_data_category.groupby('Pays/Région')['Quantité'].sum().reset_index()
+
+# Créer le graphique en secteur avec Plotly Express
+fig = px.pie(quantity_by_country, values='Quantité', names='Pays/Région', title=f"Quantités vendues par pays - Catégorie: {selected_category}")
+st.plotly_chart(fig, use_container_width=True)
