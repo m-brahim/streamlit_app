f74f6f19f93a3bf43612f60120f51f04c81e7a81
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-04 14:28:11 +0100

    Update 1_pays.py

--- pages/1_pays.py
+++ pages/1_pays.py
@@ -11,8 +11,8 @@ df['Ventes'] = pd.to_numeric(df['Ventes'], errors='coerce', downcast='integer')
 # Titre de la page
 st.set_page_config("Suivi géographique des ventes", page_icon="", layout="wide")
 
-# Colonne pour le titre à l'extrême droite
-col_title, col_dropdown = st.columns([3, 1])  # Ajustez les proportions en conséquence
+# Colonne pour le titre à l'extrême gauche
+col_title, col_dropdown = st.columns([3, 1])
 
 with col_title:
     st.subheader("Suivi géographique des ventes")
@@ -54,26 +54,28 @@ col_orders.metric(label=f"Nombre total de commandes pour {selected_country}", va
 
 st.header("")
 
-col_title2, col_dropdown2 = st.columns([3, 1])  # Ajustez les proportions en conséquence
+# Colonne pour la visualisation à gauche
+col_visualization, col_others = st.columns(2)
 
-with col_title2:
+with col_visualization:
     st.subheader("Visualisation")
 
-# Liste déroulante à côté du titre
-with col_dropdown2:
+    # Liste déroulante à côté du titre
     selected_category = st.selectbox("Sélectionnez une catégorie", df['Catégorie'].unique())
 
-# Filtrer les données par catégorie
-filtered_data_category = df[df['Catégorie'] == selected_category]
+    # Filtrer les données par catégorie
+    filtered_data_category = df[df['Catégorie'] == selected_category]
 
-# Calculer les quantités vendues par pays pour la catégorie sélectionnée
-quantity_by_country = filtered_data_category.groupby('Pays/Région')['Quantité'].sum().reset_index()
+    # Calculer les quantités vendues par pays pour la catégorie sélectionnée
+    quantity_by_country = filtered_data_category.groupby('Pays/Région')['Quantité'].sum().reset_index()
 
-# Créer le graphique en secteur avec Plotly Express
-fig = px.pie(quantity_by_country, values='Quantité', names='Pays/Région', title=f"Quantités vendues par pays - Catégorie: {selected_category}")
-st.plotly_chart(fig, use_container_width=True)
+    # Créer le graphique en secteur avec Plotly Express
+    fig = px.pie(quantity_by_country, values='Quantité', names='Pays/Région', title=f"Quantités vendues par pays - Catégorie: {selected_category}")
+    st.plotly_chart(fig, use_container_width=True)
 
-
-
+# Colonne pour les autres visualisations à droite
+with col_others:
+    st.subheader("Autres Visualisations")
+    # Ajoutez d'autres visualisations à droite de votre choix
 
 
