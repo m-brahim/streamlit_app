f2efa885ec8bb1d95a3699578ed85d7438a83374
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-05 11:51:02 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -1,1 +1,29 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import folium
+from streamlit_folium import st_folium
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
 
+url = "Exemple - Hypermarché_Achats.csv"
+
+df = pd.read_csv(url, delimiter=";")
+df['Ventes'] = df['Ventes'].str.replace('[^\d]', '', regex=True)
+df['Ventes'] = pd.to_numeric(df['Ventes'], errors='coerce', downcast='integer')
+
+# Ajouter une colonne pour l'année à partir de la colonne de dates de commande
+df['Année'] = pd.to_datetime(df['Date de commande'], format='%d/%m/%Y').dt.year
+
+# Titre de la page
+st.set_page_config("Suivi temporel des ventes", page_icon="", layout="wide")
+
+# Colonne pour le titre à l'extrême droite
+col_title, col_dropdown = st.columns([3, 1])  # Ajustez les proportions en conséquence
+
+with col_title:
+    st.subheader("Suivi temporel des ventes")
+
+# Liste déroulante à côté du titre
+with col_dropdown:
+    selected_year = st.selectbox("Sélectionnez une année", df['Année'].unique())
