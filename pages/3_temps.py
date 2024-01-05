9b52457d44b771843ce0140f2ba358beb4d4ace4
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-05 14:43:49 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -15,9 +15,6 @@ df['Ventes'] = pd.to_numeric(df['Ventes'], errors='coerce', downcast='integer')
 # Ajouter une colonne pour l'année à partir de la colonne de dates de commande
 df['Année'] = pd.to_datetime(df['Date de commande'], format='%d/%m/%Y').dt.year
 
-# Obtenir les années triées
-sorted_years = sorted(df['Année'].unique())
-
 # Titre de la page
 st.set_page_config("Suivi temporel des ventes", page_icon="", layout="wide")
 
@@ -28,9 +25,9 @@ with col_title:
     st.subheader("Suivi temporel des ventes")
 
     # Ajouter le premier métrique (nombre de clients)
-    num_clients = df[df['Année'] == selected_year]['ID client'].unique()
+    num_clients = df[df['Année'] == selected_year]['ID client'].nunique()
     st.metric(label="Nombre de clients", value=num_clients)
 
 # Liste déroulante à côté du titre
 with col_dropdown:
-    selected_year = st.selectbox("Sélectionnez une année", sorted_years)
+    selected_year = st.selectbox("Sélectionnez une année", df['Année'].unique())
