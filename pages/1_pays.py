6086bddc268453023531c3e53b186921f025b2e2
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-09 14:55:55 +0100

    Update 1_pays.py

--- pages/1_pays.py
+++ pages/1_pays.py
@@ -81,7 +81,7 @@ client_icon = folium.CustomIcon(icon_image=icon_path, icon_size=(30, 30))
 # Ajoutez une carte Folium avec une taille spécifique
 with col_map:
     st.subheader("Nombre de clients par pays")
-    my_map = folium.Map(location=[merged_data['Latitude'].iloc[0], merged_data['Longitude'].iloc[0]], zoom_start=8)
+    my_map = folium.Map(location=[merged_data['Latitude'].iloc[0], merged_data['Longitude'].iloc[0]], zoom_start=6)
     
     # Ajoutez une seule marqueur pour représenter le pays avec le nombre de clients dans l'infobulle
     folium.Marker([merged_data['Latitude'].iloc[0], merged_data['Longitude'].iloc[0]], 
