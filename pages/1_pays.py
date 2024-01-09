c5408ed9363ec9123b3701c8e2119836874970c4
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-09 13:53:12 +0100

    Update 1_pays.py

--- pages/1_pays.py
+++ pages/1_pays.py
@@ -85,4 +85,4 @@ with col_map:
                   icon=folium.Icon(color='blue')).add_to(my_map)
     
     # Affichez la carte avec Streamlit Folium
-    st_folium(my_map, width = 500, height = 500)
+    st_folium(my_map, width = 700, height = 500)
