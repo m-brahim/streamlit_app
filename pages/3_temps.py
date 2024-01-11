7f7b1ebf757f99b2187cee068a785f235b6c195a
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-11 10:53:46 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -15,9 +15,8 @@ style = """
 <style>
 [class="main st-emotion-cache-uf99v8 ea3mdgi3"]
 {
-background-color: #e5e5f7;
-opacity: 0.8;
-background: repeating-linear-gradient( 45deg, #71767c, #71767c 5px, #e5e5f7 5px, #e5e5f7 25px );
+background-image : url(https://www.pexels.com/photo/blue-sky-281260/);
+background-size : cover;
 }
 </style>
 """
