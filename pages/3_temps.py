ef0d7d7b87b30a89b617446ede5ac71f190a2fd6
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-16 16:07:05 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -73,6 +73,9 @@ with col_h1:
     st.header("Données utilisées")
 
 
+
+
+
 #tableau
 
 # Charger les données depuis le fichier CSV
@@ -87,15 +90,15 @@ col_space, col_country, col_space, col_category, col_space, col_client, col_spac
 
 # Liste déroulante pour le pays
 with col_country:
-    selected_country = st.selectbox('Sélectionnez le pays', df_table['Pays/Région'].unique())
+    selected_country = st.selectbox('Sélectionnez le pays', [''] + df_table['Pays/Région'].unique())
 
 # Liste déroulante pour la catégorie
 with col_category:
-    selected_category = st.selectbox('Sélectionnez la catégorie', df_table['Catégorie'].unique())
+    selected_category = st.selectbox('Sélectionnez la catégorie', [''] + df_table['Catégorie'].unique())
 
 # Liste déroulante pour le client
 with col_client:
-    selected_client = st.selectbox('Sélectionnez le client', df_table['Nom du client'].unique())
+    selected_client = st.selectbox('Sélectionnez le client', [''] + df_table['Nom du client'].unique())
 
 # Appliquer les filtres
 df_filtre = df_table[
@@ -130,6 +133,11 @@ fig_table.update_layout(
 
 # Afficher les listes déroulantes et le tableau
 st.plotly_chart(fig_table, use_container_width=True)
+
+
+
+
+
 
 
 #PARTIE KPI
