3414598955013d768a8d5c69697a55d7fcbb4ad3
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-16 12:19:07 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -212,7 +212,6 @@ with col_v2:
                                       xaxis=dict(title='Nombre de commandes', tickfont=dict(size=15), title_font=dict(size=18)),
                                       yaxis=dict(title='Mois', tickfont=dict(size=15), title_font=dict(size=18)),
                                       title_font=dict(size=20),
-                                      textposition = 'outside',
                                       height=600,
                                       width=600)
     
