f853d873414b1ec0ed8a730cd59c1c5813b5d0e2
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-16 10:27:25 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -174,14 +174,13 @@ with col_v2:
     monthly_orders_selected_year = filtered_df[filtered_df['Année'] == selected_year].groupby('Mois')['ID commande'].count().reset_index()
     
     # Triez les mois dans l'ordre décroissant du nombre de commandes pour l'année sélectionnée
-    monthly_orders_selected_year = monthly_orders_selected_year.sort_values(by='ID commande', ascending=False)
 
     # Agréger le nombre de commandes par mois pour l'année de comparaison
     monthly_orders_comparison_year = filtered_df[filtered_df['Année'] == selected_comparison_year].groupby(
         'Mois')['ID commande'].count().reset_index()
 
     # Triez les mois dans l'ordre décroissant du nombre de commandes pour l'année de comparaison
-    
+    monthly_orders_comparison_year = monthly_orders_comparison_year.sort_values(by='ID commande', ascending=True)
 
     # Affiche l'évolution du nombre de commandes pour N-*
     fig_orders_evolution.add_trace(go.Bar(
