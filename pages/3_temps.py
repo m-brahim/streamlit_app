8d683dc9b058f3e0a8055860e388fc7ef4ae4b4e
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-18 09:16:39 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -265,9 +265,9 @@ with col_v2:
                                       xaxis=dict(title='Nombre de commandes', tickfont=dict(size=15), title_font=dict(size=18)),
                                       yaxis=dict(title='Mois', tickfont=dict(size=15), title_font=dict(size=18)),
                                       title_font=dict(size=20),
-                                       title_x = 0.2
-                                       height=graph_height,
-                                       width=graph_width)
+                                      title_x = 0.2
+                                      height=graph_height,
+                                      width=graph_width)
     
     # Affichage
     st.plotly_chart(fig_orders_evolution, use_container_width=True)
