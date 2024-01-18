28357025a2d8f3083a42c36e046781e542b5a3fb
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-18 09:16:11 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -210,7 +210,7 @@ with col_v1:
                                        xaxis=dict(title='Mois', tickfont=dict(size=15), title_font=dict(size=18)),
                                        yaxis=dict(title='Nombre de clients', tickfont=dict(size=15), title_font=dict(size=18)),
                                        title_font=dict(size=20),
-                                       title_x = 0.1,
+                                       title_x = 0.2,
                                        height=graph_height,
                                        width=graph_width)
     
@@ -265,6 +265,7 @@ with col_v2:
                                       xaxis=dict(title='Nombre de commandes', tickfont=dict(size=15), title_font=dict(size=18)),
                                       yaxis=dict(title='Mois', tickfont=dict(size=15), title_font=dict(size=18)),
                                       title_font=dict(size=20),
+                                       title_x = 0.2
                                        height=graph_height,
                                        width=graph_width)
     
