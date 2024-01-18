008f93f3fc581806197669bf51091aca508feebf
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-18 11:48:32 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -171,7 +171,7 @@ style_metric_cards()
 
 
 #création de colonnes et attribution de dimensions
-col_txt, col_v1, col_v2 = st.columns([1,2,2])
+col_txt, col_v1, col_v2 = st.columns([0.5,2,2])
 
 with col_txt:
     st.write("*Graphiques*:")
