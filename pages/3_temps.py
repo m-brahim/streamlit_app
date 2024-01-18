25035c146491c55b4ca268eb1857e4629d32edd9
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-18 10:35:22 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -136,7 +136,8 @@ col_txt, col_sp1, col_clients, col_sp2, col_orders, col_sp3, col_ca, col_sp4= st
 
 
 with col_txt:
-    st.markdown("Chiffres clés N vs N-* : ")
+    st.markdown("***Chiffres clés N vs N-* : ***")
+
 
 #calculs
 num_clients = df[df['Année'] == selected_year].drop_duplicates('ID client')['ID client'].count()
