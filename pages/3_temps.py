4648713ea410b9a9db529760932c3d5e82e3544f
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-19 11:51:35 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -146,7 +146,7 @@ def plot_top_products_by_country(df, selected_country):
     top_products = filtered_data.groupby('Nom du produit')['Quantité'].sum().reset_index()
 
     # Trier par quantité décroissante et sélectionner les 5 premiers produits
-    top_products = top_products.sort_values(by='Quantité', ascending=False).head(5)
+    top_products = top_products.sort_values(by='Quantité', ascending=True).head(5)
 
     rc = {'figure.figsize': (8, 5),  # Ajustez la hauteur à 5 pouces
           'axes.facecolor': '#eff1f5',
