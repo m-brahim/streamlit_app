bde087c0b626083309d62ca37562f68c472184ca
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-25 18:30:21 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -418,7 +418,8 @@ with col_v2:
                                        title_font=dict(size=15),
                                        title_x=0.2,
                                        height=graph_height,
-                                       width=graph_width)
+                                       width=graph_width,
+                                       bargap=0.2)
 
     # Affichage
     st.plotly_chart(fig_orders_evolution, use_container_width=True)
