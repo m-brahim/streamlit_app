4464e418fb58dc732016045fbfc016b74153a456
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-25 17:17:30 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -362,10 +362,10 @@ with col_v2:
     fig_orders_evolution.add_shape(
         go.layout.Shape(
             type="line",
-            x0=target_value,
-            x1=target_value,
-            y0=monthly_orders_selected_year['Mois'].min(),
-            y1=monthly_orders_selected_year['Mois'].max(),
+            x0=monthly_orders_selected_year['Mois'].min(),
+            x1=monthly_orders_selected_year['Mois'].max(),
+            y0=target_value,
+            y1=target_value,
             line=dict(color="red", width=2, dash="dash"),
             xref='x'
         )
