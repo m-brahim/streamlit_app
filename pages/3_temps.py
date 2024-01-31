27497e3ec7e801c70c60eedf89feb542b52f8710
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-01-31 09:28:34 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -413,6 +413,10 @@ with col_map:
 
         st_folium(my_map, width=800, height=700)
 
+        m2 = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+        folium.Marker([39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell").add_to(m)
+        st_folium(m2, width=725)
+
 
 
 with col_class :
