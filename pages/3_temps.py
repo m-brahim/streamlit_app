799e5f16a7a5662047b91cf4f19743aa7cbac183
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-01 15:25:17 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -53,7 +53,7 @@ with col_title:
         
 with col_logo:
     logo = "pages/Kiloutou_logo.jpg"
-    st.image(logo, width=100)
+    st.image(logo, width=80)
 
 with st.sidebar:
     st.header("Paramètres des graphiques")
