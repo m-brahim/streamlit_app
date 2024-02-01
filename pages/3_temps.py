5ef6e19d8d3be41fbc65acbc3f91d79cb5478664
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-01 09:57:34 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -53,6 +53,10 @@ with st.sidebar:
     graph_height = st.slider("Hauteur des graphiques", min_value=300, max_value=1200, value=500)
 
 
+
+
+
+
 #PARTIE Vis'
 
 #1) analyse client
@@ -61,6 +65,9 @@ col_h1, col2, col3 = st.columns([1,1,1])
 
 with col_h1:
     st.header("1. Analyse client")
+
+
+
 
 
 # tableau
@@ -112,23 +119,18 @@ if selection_effectuee:
     # Réappliquer les filtres après la modification de la colonne 'Ventes'
     df_filtre = df[(df['Pays/Région'] == selected_country) & (df['Catégorie'] == selected_category) & (df['Nom du client'] == selected_client)]
 
-    # Créer une copie du DataFrame pour ajouter des styles
-    styled_df = df_filtre[selected_columns_table].copy()
-    
     # Trouver l'indice de la cellule avec la vente la plus élevée
     max_sales_index = df_filtre['Ventes'].idxmax()
-    
-    # Ajouter une colonne temporaire pour l'index
-    styled_df['temp_index'] = styled_df.index
-    
-    # Appliquer le style à l'ensemble de la ligne ayant la plus grande vente
-    styled_df = styled_df.apply(lambda row: ['background: green; color: white' if row['temp_index'] == max_sales_index else '' for _ in row], axis=1)
-    
-    # Supprimer la colonne temporaire
-    styled_df.drop('temp_index', axis=1, inplace=True)
-    
+
+    # Créer une copie du DataFrame pour ajouter des styles
+    styled_df = df_filtre[selected_columns_table].style.apply(lambda row: ['background: green' if row.name == max_sales_index else '' for _ in row], axis=1)
+
     # Afficher le tableau avec les styles
     st.table(styled_df)
+
+
+
+
 
 
 
