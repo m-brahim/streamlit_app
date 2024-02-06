b628665a0ee4d2e1e44a37589765302cc2c55352
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-06 10:09:15 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -71,15 +71,7 @@ with st.sidebar:
         manual_select = st.session_state['menu_option']
     else:
         manual_select = None
-    # 5. Add on_change callback
-    def on_change(key):
-        selection = st.session_state[key]
-        st.write(f"Selection changed to {selection}")
-        
-    selected5 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'],
-                            icons=['house', 'cloud-upload', "list-task", 'gear'],
-                            on_change=on_change, key='menu_5', orientation="horizontal")
-    selected5
+    
 
 
 
