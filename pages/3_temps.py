6115e9e0bdb907704ff00464e77e4c4a847bf209
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-06 10:10:58 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -57,7 +57,7 @@ with col_logo:
     st.image(logo, width=73)
 
 with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Upload', 'Tasks', 'Settings'], 
+    selected = option_menu("Main Menu", ["Accueil", 'Import', 'Tâches', 'Paramètres'], 
         icons=['house', 'cloud-upload', "list-task", 'gear'], menu_icon="cast", default_index=0)
     styles={
         "container": {"padding": "0!important", "background-color": "#fafafa"},
