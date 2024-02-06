aff0c9f3f3dfb1e856eea81d29f463225caab92a
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-06 10:26:52 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -61,8 +61,8 @@ with st.sidebar:
     icons=['house', 'cloud-upload', "list-task", 'gear'], 
     menu_icon="cast", default_index=0,
     styles={
-        "container": {"padding": "0!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "25px"}, 
+        "container": {"padding": "0!important", "background-color": "#f3bd1d"},
+        "icon": {"color": "black", "font-size": "25px"}, 
         "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
         "nav-link-selected": {"background-color": "green"},
     }
