7eb8b2dccaf9144a7edd3eb2b44515a764d2e9c8
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-06 10:08:41 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -57,46 +57,29 @@ with col_logo:
     st.image(logo, width=73)
 
 with st.sidebar:
-    st.header("Paramètres des graphiques")
-    graph_height = st.slider("Hauteur des graphiques", min_value=300, max_value=1200, value=500)
-    selected = option_menu("Main Menu", ["Home", 'Settings'], 
-        icons=['house', 'gear'], menu_icon="cast", default_index=1)
-    selected
-
-
-selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    menu_icon="cast", default_index=0, orientation="horizontal",
+    selected = option_menu("Main Menu", ["Home", 'Upload', 'Tasks', 'Settings'], 
+        icons=['house', 'cloud-upload', "list-task", 'gear'], menu_icon="cast", default_index=0)
     styles={
         "container": {"padding": "0!important", "background-color": "#fafafa"},
         "icon": {"color": "orange", "font-size": "25px"}, 
         "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
         "nav-link-selected": {"background-color": "green"},
     }
-)
-
-# 4. Manual item selection
-if st.session_state.get('switch_button', False):
-    st.session_state['menu_option'] = (st.session_state.get('menu_option', 0) + 1) % 4
-    manual_select = st.session_state['menu_option']
-else:
-    manual_select = None
-    
-selected4 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    orientation="horizontal", manual_select=manual_select, key='menu_4')
-st.button(f"Move to Next {st.session_state.get('menu_option', 1)}", key='switch_button')
-selected4
-
-# 5. Add on_change callback
-def on_change(key):
-    selection = st.session_state[key]
-    st.write(f"Selection changed to {selection}")
-    
-selected5 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'],
-                        icons=['house', 'cloud-upload', "list-task", 'gear'],
-                        on_change=on_change, key='menu_5', orientation="horizontal")
-selected5
+    selected
+    if st.session_state.get('switch_button', False):
+        st.session_state['menu_option'] = (st.session_state.get('menu_option', 0) + 1) % 4
+        manual_select = st.session_state['menu_option']
+    else:
+        manual_select = None
+    # 5. Add on_change callback
+    def on_change(key):
+        selection = st.session_state[key]
+        st.write(f"Selection changed to {selection}")
+        
+    selected5 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'],
+                            icons=['house', 'cloud-upload', "list-task", 'gear'],
+                            on_change=on_change, key='menu_5', orientation="horizontal")
+    selected5
 
 
 
