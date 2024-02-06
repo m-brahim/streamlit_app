e1c209a7716c63cb49661510cddfba8892a091e5
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-06 17:03:23 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -183,7 +183,6 @@ if selection_effectuee:
 
         fig_gauge.update_layout(
             height=200,
-            margin=dict(l=10, r=10, t=50, b=10, pad=50),
             font=dict(size=16)
         )
         
