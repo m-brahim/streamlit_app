4ee700cb2d4b77f98dbc60baa0c5e8509a19ec29
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-07 11:02:06 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -558,7 +558,7 @@ with col_class:
             title_font=dict(size=15),
             height=300,
             width=300,
-            margin=dict(t=10, b=50)
+            margin=dict(t=20, b=50)
         )
         
         st.plotly_chart(fig, use_container_width=True)
@@ -578,7 +578,7 @@ with col_pie :
                       title_font=dict(size=15),
                       height=300,
                       width=300,
-                      margin=dict(t=10, b=50)
+                      margin=dict(t=20, b=50)
 
     )
     
