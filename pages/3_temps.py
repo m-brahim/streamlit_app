671c852d4fb34f409f1ac8c2ffa1f75e5771d514
m-brahim <155063113+m-brahim@users.noreply.github.com>
2024-02-07 11:03:30 +0100

    Update 3_temps.py

--- pages/3_temps.py
+++ pages/3_temps.py
@@ -578,7 +578,7 @@ with col_pie :
                       title_font=dict(size=15),
                       height=300,
                       width=300,
-                      margin=dict(t=20, b=50, l=100)
+                      margin=dict(t=20, b=50, l=100, pad=10)
 
     )
     
